1d88ee9a913453d29bf5a2e5a3ca2f51eeb7dece
DESKTOP-UC0K0TL\DELL <deepak19992012@gmail.com>
2025-01-27 17:06:19 +0530

    2 project added, hello world and forloops

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While loops
+            /*for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
